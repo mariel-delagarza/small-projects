a919116c0ff9c77fb349d30bb18dcf52b277ee61
Mariel de la Garza <mariel.code@gmail.com>
2025-08-07 11:32:52 -0400

    grid of states

---
+++ misc-examples/national-small-multiples/charts/chart42.js
@@ -0,0 +1,61 @@
+Highcharts.chart("chart42", {
+  title: {
+    text: "",
+    align: "left",
+  },
+  legend: {
+    enabled: false,
+  },
+  subtitle: {
+    text: "",
+    align: "left",
+  },
+
+  yAxis: {
+    title: {
+      text: "",
+    },
+  },
+
+  xAxis: {
+    accessibility: {
+      rangeDescription: "Range: 2010 to 2022",
+    },
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false,
+      },
+      pointStart: 2010,
+    },
+  },
+
+  series: [
+    {
+      name: "",
+      data: [40, 48, 65, 82, 33, 28, 67],
+    },
+  ],
+
+  credits: {
+    enabled: false,
+  },
+  responsive: {
+    rules: [
+      {
+        condition: {
+          maxWidth: 500,
+        },
+        chartOptions: {
+          legend: {
+            layout: "horizontal",
+            align: "center",
+            verticalAlign: "bottom",
+          },
+        },
+      },
+    ],
+  },
+});
